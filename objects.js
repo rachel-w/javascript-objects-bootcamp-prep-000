2d37ad62b36e896b159e642dc2182bca4e6b202f
Rachel Wey <rachelwey96@outlook.com>
2018-10-25 06:28:05 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,4 +1,4 @@
-var playlist = new Object ({})
+var playlist = new Object ({[artistName]:songTitle})
 
 function updatePlaylist(myPlaylist, artistName, songTitle){
   return Object.assign({}, myPlaylist,{[artistName]:songTitle})
