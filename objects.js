ed3e9a51bbedaa04e4427e6242873630d276e76c
Rachel Wey <rachelwey96@outlook.com>
2018-10-25 06:23:33 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -6,7 +6,11 @@ function updatePlaylist(myPlaylist, artistName, songTitle){
   return Object.assign({}, myPlaylist,{[artistName]:songTitle})
 }
 
+updatePlaylist(playlist, "Voidz", "Permanent High School")
+
 function removeFromPlaylist(myPlaylist, artistName){
   delete myPlaylist[artistName]
   return myPlaylist
 }
+
+removeFromPlaylist(playlist,"Soko")
