55b0f44ac950ffe6f467ed41e2d7e3cbb7d12d23
Rachel Wey <rachelwey96@outlook.com>
2018-10-25 06:08:47 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,7 +1,7 @@
 var playlist = new Object ({StillWoozy: "Goodie Bag"})
 
 function updatePlaylist(playlist, artistName, songTitle){
-  return Object.assign({}, playlist,{}[artistName]:songTitle})
+  return Object.assign({}, playlist,{[artistName]:songTitle})
 }
 
 function removeFromPlaylist(playlist, artistName){
